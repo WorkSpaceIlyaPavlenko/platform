708e5bd35548c92b0f588a74296f95ee4fc1b498
robolox344 <ilya.pavlenkotio@mail.ru>
2025-08-09 23:51:47 +0300

    u[date api front

--- frontend/src/features/order/components/OrderApplicationForm.tsx
+++ frontend/src/features/order/components/OrderApplicationForm.tsx
@@ -25,7 +25,7 @@ export default function OrderApplicationForm(){
         setStatus(newStatus)
 
         try {
-            const res = await fetch("http://localhost:8080/api/telegram/form", {
+            const res = await fetch("/api/telegram/form", {
                 method: "POST",
                 headers: {
                 "Content-Type": "application/json"
